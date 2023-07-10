0358ec20a90428eb497569e0c835340667a8dc87
Michal Przytulski <michal@przytulski.pl>
2023-07-10 18:49:19 +0200

    update deps

--- src/main.ts
+++ src/main.ts
@@ -114,7 +114,7 @@ const main = async () => {
 
       if (issues.length) {
         const extendIssues = (
-          rawIssues: Issue[]
+          rawIssues: Issue[],
         ): Promise<FoundIssueType[]> => {
           const promises = rawIssues.map(
             async (issue): Promise<FoundIssueType> => {
@@ -124,7 +124,7 @@ const main = async () => {
                 labels: inputs.withLabels ? (await issue.labels()).nodes : null,
                 project: inputs.withProject ? await issue.project : null,
               };
-            }
+            },
           );
 
           return Promise.all(promises);
@@ -144,7 +144,7 @@ const main = async () => {
     }
 
     setFailed(
-      `Failed to find Linear issue identifier in PR branch, title, or body.`
+      `Failed to find Linear issue identifier in PR branch, title, or body.`,
     );
     return;
   } catch (error) {
